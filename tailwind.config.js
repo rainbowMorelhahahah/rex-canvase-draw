847abd83422293ea54b214aa71cc461d7ddd1784
rex <rainbowmorel@163.com>
2023-06-21 19:02:56 +0800

    feat:  提交部分代码

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/*.tsx',
+    './src/components/**/*.tsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("tailwindcss-radix")()
+  ],
+}
+
